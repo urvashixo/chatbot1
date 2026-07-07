e849d1feb626c472d331f4d538e866fb1604e5d5
Urvashi Chawla <urvashichawla0905@gmail.com>
2026-07-07 16:39:50 +0530

    added chatbotassistent function with intent probabilities

--- main.py
+++ main.py
@@ -16,4 +16,30 @@ class ChatbotModel(nn.Module):
         self.fc1 = nn.Linear(input_size, 128) #128 neurons
         self.fc2 = nn.Linear(128, 64) #64 neurons
         self.fc3 = nn.Linear(64, output_size) #output_size neurons
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU() #Breaks the linearity of the model and allows it to learn complex patterns
+        self.dropout = nn.Dropout(0.5) # Dropout layer with 50% probability for regularization
+
+    def forward(self, x):
+        x=self.relu(self.fc1(x))
+        x=self.dropout(x) # Apply dropout after the first layer
+        x=self.relu(self.fc2(x))
+        x=self.dropout(x) # Apply dropout after the second layer
+        x=self.fc3(x) # No activation function for the output layer
+    
+class ChatbotAssistant:
+    def __init__(self, intents_path, function_mappings = None):
+        self.model=None
+        self.intents_path=intents_path
+        self.documents=[]
+        self.vocabulary=[]
+        self.intents= [] #probabilities for each intent
+        self.intents_responses=[] #responses for each intent
+
+        self.function_mappings = function_mappings
+        self.X= None #X is matrix
+        self.y= None #y is a vector
+
+    @staticmethod
+    def tokenize_and_lemmatize(text):
+        lemmatizer = nltk.WordNetLemmatizer()
+        words = nltk.word_tokenize(text)
